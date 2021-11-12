dec0b3ca215602e817ddb8040c485d82dd32eb1c
Viktor <viktorglemne@gmail.com>
2021-11-12 10:13:44 +0100

    styling ändring login, signup

--- login.php
+++ login.php
@@ -5,19 +5,18 @@ require_once 'classes/config.php';
 require_once "classes/component.php";
 $titel = "Log in | WEfit Bäst på kosttilskott";
 $html_login = file_get_contents("html/login.html");
-// menu($titel);
-
-echo $html_login;
+menu($titel);
 
 try {
     if (!isset($_SESSION['username'])) {
+        echo $html_login;
         $userEmail = $_POST["email"];
         $userPass = $_POST["pass"];
         $stmt = $pdo->query("SELECT * FROM customer WHERE email='$userEmail' AND pass = '$userPass';");
         $row = $stmt->fetch();
         if ($stmt->rowCount() < 1) {
             $_SESSION['username'] = NULL;
-            throw new Exception('User not found');
+            // throw new Exception('User not found');
         } else {
             echo "You have successfully logged in as ", $row['email'];
             $_SESSION["email"] = $row['id'];
